61c65246b56d47f5def9da3e76ab398fa5b00fbb
Isaac Joshua <isaacjcarnes@gmail.com>
2021-09-18 16:55:21 -0400

    Added prompts and verification for password settings.

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,9 @@
+var characters = {
+  lowercase: ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"],
+  uppercase: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
+  numbers: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
+  specialCharacters: ['!','@','#','$','%','&','*','?']
+}
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -8,6 +14,43 @@ function writePassword() {
 
   passwordText.value = password;
 
+}
+
+function generatePassword(){
+  var passwordLength = 0;
+  var password = "";
+  var possibleCharacters = [];
+  
+  passwordLength = prompt("How long would you like your password to be?\n(8-128 characters)");
+  if(passwordLength > 128){
+    alert(passwordLength + " is too many characters.\nPassword will be 128 characters.");
+    passwordLength = 128;
+  } else if(passwordLength < 8){
+    alert(passwordLength + " is too little characters.\nPassword will be 8 characters.");
+    passwordLength = 8;
+  }
+
+  if(confirm("Do you want to use lowercase characters?", " ")){
+    possibleCharacters = possibleCharacters.concat(characters.lowercase);
+  }
+  if(confirm("Do you want to use uppercase characters?", " ")){
+    possibleCharacters = possibleCharacters.concat(characters.uppercase);
+  }
+  if(confirm("Do you want to use number characters?", " ")){
+    possibleCharacters = possibleCharacters.concat(characters.numbers);
+  }
+  if(confirm("Do you want to use special characters?", " ")){
+    possibleCharacters = possibleCharacters.concat(characters.specialCharacters);
+  }
+
+  if(possibleCharacters.length == 0){
+    alert("No characters were selected for use.\nLowercase and uppercase characters will be added to the password.");
+    possibleCharacters = possibleCharacters.concat(characters.lowercase);
+    possibleCharacters = possibleCharacters.concat(characters.uppercase);
+  }
+
+  console.log(possibleCharacters);
+  return password;
 }
 
 // Add event listener to generate button
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # password-generator-ij
+
+Checklist:
+Create prompts when generate password button is clicked: done
+Prompts for password length: done
+Prompts for if password includes lowercase characters: done
+Prompts for if password includes uppercase characters: done
+Prompts for if password includes number characters: done
+Prompts for if password includes special characters: done
+Validate prompt selection, do not allow selecting no characters or invalid password length: done
+Generate and return password
+Either display password on page or in alert
